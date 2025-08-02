c3f412f2d6c2cd16608df6ac72dcc4c3ab39c3d0
darcy <acednes@gmail.com>
2025-08-02 11:46:13 +1000

    refactor: clean up `Parser`

--- src/Parser.zig
+++ src/Parser.zig
@@ -10,6 +10,8 @@ const TermIndex = model.TermIndex;
 const TermStore = model.TermStore;
 const Term = model.Term;
 
+const TermError = error{ UnexpectedToken, UnexpectedEol, OutOfMemory };
+
 tokens: TokenBuf,
 
 pub fn new(text: []const u8, stmt: Span) !Self {
@@ -18,97 +20,27 @@ pub fn new(text: []const u8, stmt: Span) !Self {
     };
 }
 
-fn peek(self: *Self) ?Token {
-    return self.tokens.peek();
-}
-
-fn tryNext(self: *Self) ?Token {
-    return self.tokens.next();
-}
-
-fn expectNext(self: *Self) !Token {
-    return self.tryNext() orelse return error.UnexpectedEol;
-}
-
-pub fn expectIdent(self: *Self) !Span {
-    const token = try self.expectNext();
-    if (token.kind != .Ident) {
-        return error.UnexpectedToken;
-    }
-    return token.span;
-}
-
-pub fn tryIdent(self: *Self) !?Span {
-    const token = self.tryNext() orelse return null;
-    if (token.kind != .Ident) {
-        return null;
-    }
-    return token.span;
-}
-
-pub fn expectIdentOrEol(self: *Self) !?Span {
-    const token = self.tryNext() orelse return null;
-    if (token.kind != .Ident) {
-        return error.UnexpectedToken;
-    }
-    return token.span;
-}
-
-// TODO(refactor): Use `End` instead of `Eol`
-pub fn expectEol(self: *Self) !void {
-    if (!self.tokens.isEnd()) {
-        return error.UnexpectedToken;
-    }
-}
-
-pub fn expectEquals(self: *Self) !void {
-    const token = try self.expectNext();
-    if (token.kind != .Equals) {
-        return error.UnexpectedToken;
-    }
-}
-pub fn expectDot(self: *Self) !void {
-    const token = try self.expectNext();
-    if (token.kind != .Dot) {
-        return error.UnexpectedToken;
-    }
-}
-pub fn expectParenRight(self: *Self) !Span {
-    const token = try self.expectNext();
-    if (token.kind != .ParenRight) {
-        return error.UnexpectedToken;
-    }
-    return token.span;
-}
-
-const TermError = error{ UnexpectedToken, UnexpectedEol, OutOfMemory };
-
 pub fn tryDeclaration(self: *Self, store: *TermStore) TermError!?Decl {
-    const name = try self.expectIdentOrEol() orelse {
+    const name = try self.expectIdentOrEnd() orelse {
         return null;
     };
-    try self.expectEquals();
-    const term_index = try self.expectStatementTerm(store);
+    _ = try self.expectTokenKind(.Equals);
+    const index = try self.expectTerm(store, true);
+    try self.expectEnd();
 
     return Decl{
         .name = name,
-        .term = term_index,
+        .term = index,
     };
 }
 
-pub fn expectStatementTerm(self: *Self, store: *TermStore) TermError!TermIndex {
-    const index = try self.expectTerm(store, true);
-    try self.expectEol();
-    return index;
-}
-
-pub fn expectTerm(self: *Self, store: *TermStore, is_greedy: bool) TermError!TermIndex {
+fn expectTerm(self: *Self, store: *TermStore, comptime is_greedy: bool) TermError!TermIndex {
     return try self.tryTerm(store, is_greedy) orelse {
         return error.UnexpectedEol;
     };
 }
 
-pub fn tryTerm(self: *Self, store: *TermStore, is_greedy: bool) !?TermIndex {
+fn tryTerm(self: *Self, store: *TermStore, comptime is_greedy: bool) !?TermIndex {
     const first = self.tryNext() orelse {
         return null;
     };
@@ -119,22 +51,19 @@ pub fn tryTerm(self: *Self, store: *TermStore, is_greedy: bool) !?TermIndex {
                 .variable = first.span,
             });
 
-            while (is_greedy) {
-                if (self.peek()) |token| {
-                    if (token.kind == .ParenRight) {
-                        break;
-                    }
-                }
+            if (!is_greedy) {
+                return index;
+            }
 
+            while (!self.peekIsTokenKind(.ParenRight)) {
                 const right_index = try self.tryTerm(store, false) orelse {
                     break;
                 };
-                const right = store.get(right_index);
-                const span = first.span.join(right.getSpan());
+                const right_span = store.get(right_index).getSpan();
 
                 index = try store.append(Term{
-                    .application = Term.Appl{
-                        .span = span,
+                    .application = .{
+                        .span = first.span.join(right_span),
                         .left = index,
                         .right = right_index,
                     },
@@ -144,16 +73,15 @@ pub fn tryTerm(self: *Self, store: *TermStore, is_greedy: bool) !?TermIndex {
         },
 
         .Backslash => {
-            const variable = try self.expectIdent();
-            try self.expectDot();
+            const variable = try self.expectTokenKind(.Ident);
+            _ = try self.expectTokenKind(.Dot);
 
             const right_index = try self.expectTerm(store, true);
-            const right = store.get(right_index);
-            const span = first.span.join(right.getSpan());
+            const right_span = store.get(right_index).getSpan();
 
             return try store.append(Term{
-                .abstraction = Term.Abstr{
-                    .span = span,
+                .abstraction = .{
+                    .span = first.span.join(right_span),
                     .variable = variable,
                     .right = right_index,
                 },
@@ -162,13 +90,11 @@ pub fn tryTerm(self: *Self, store: *TermStore, is_greedy: bool) !?TermIndex {
 
         .ParenLeft => {
             const inner = try self.expectTerm(store, true);
-
-            const paren_right = try self.expectParenRight();
-            const span = first.span.join(paren_right);
+            const right_span = try self.expectTokenKind(.ParenRight);
 
             return try store.append(Term{
-                .group = Term.Group{
-                    .span = span,
+                .group = .{
+                    .span = first.span.join(right_span),
                     .inner = inner,
                 },
             });
@@ -178,4 +104,45 @@ pub fn tryTerm(self: *Self, store: *TermStore, is_greedy: bool) !?TermIndex {
             return error.UnexpectedToken;
         },
     }
+}
+
+fn expectEnd(self: *Self) !void {
+    if (!self.tokens.isEnd()) {
+        return error.UnexpectedToken;
+    }
+}
+
+fn expectIdentOrEnd(self: *Self) !?Span {
+    const token = self.tryNext() orelse return null;
+    if (token.kind != .Ident) {
+        return error.UnexpectedToken;
+    }
+    return token.span;
+}
+
+fn expectTokenKind(self: *Self, kind: Token.Kind) !Span {
+    const token = try self.expectNext();
+    if (token.kind != kind) {
+        return error.UnexpectedToken;
+    }
+    return token.span;
+}
+
+fn peekIsTokenKind(self: *Self, kind: Token.Kind) bool {
+    if (self.peek()) |token| {
+        if (token.kind == kind) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn peek(self: *Self) ?Token {
+    return self.tokens.peek();
+}
+fn tryNext(self: *Self) ?Token {
+    return self.tokens.next();
+}
+fn expectNext(self: *Self) !Token {
+    return self.tryNext() orelse return error.UnexpectedEol;
 }
