3b856ed742cf0889110a4505034cb14a851a349c
darcy <acednes@gmail.com>
2025-08-09 13:07:53 +1000

    feat(parse): allow nonascii characters

--- src/parse/TokenChar.zig
+++ src/parse/TokenChar.zig
@@ -1,31 +1,25 @@
 const Self = @This();
 const std = @import("std");
 
-value: u8,
+codepoint: u21,
 
 pub const Kind = enum {
     Atomic,
     Combining,
     Whitespace,
     Control,
-    NonAscii,
 };
 
-pub fn from(char: u21) Self {
-    // TODO(feat): Support UTF-8
-    if (char > 0xff) {
-        return .{ .value = 0xff };
-    }
-
-    return .{ .value = @intCast(char) };
+pub fn from(codepoint: u21) Self {
+    return .{ .codepoint = codepoint };
 }
 
 pub fn kind(self: *const Self) Kind {
-    return switch (self.value) {
+    return switch (self.codepoint) {
         ' ', '\t'...'\r' => .Whitespace,
         0x00...0x08, 0x0e...0x1f, 0x7f => .Control,
         '\\', '.', ',', ';', '(', ')', '[', ']', '{', '}' => .Atomic,
-        else => |char| if (char > 0x80) .NonAscii else .Combining,
+        else => .Combining,
     };
 }
 
@@ -36,5 +30,5 @@ pub fn isAtomic(self: *const Self) bool {
     return self.kind() == .Atomic;
 }
 pub fn isLinebreak(self: *const Self) bool {
-    return self.value == '\n';
+    return self.codepoint == '\n';
 }
