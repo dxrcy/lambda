e8c2b33cc403b62fc515b09e8ba97573d7e20505
darcy <acednes@gmail.com>
2025-09-12 11:18:19 +1000

    refactor: use array buffer for peeked tokens
    instead of single item

--- src/parse/TokenBuf.zig
+++ src/parse/TokenBuf.zig
@@ -1,32 +1,61 @@
 const Self = @This();
 
+const std = @import("std");
+const assert = std.debug.assert;
+
 const Context = @import("../Context.zig");
 const Span = @import("../Span.zig");
 
 const Tokenizer = @import("Tokenizer.zig");
 const Token = @import("Token.zig");
 
+const PEEK_BUFFER_SIZE = 2;
+
 tokenizer: Tokenizer,
-peeked: ?Token,
+
+peeked_tokens: [PEEK_BUFFER_SIZE]Token,
+peeked_count: usize,
 
 pub fn new(tokenizer: Tokenizer) Self {
-    return .{ .tokenizer = tokenizer, .peeked = null };
+    return .{
+        .tokenizer = tokenizer,
+        .peeked_tokens = undefined,
+        .peeked_count = 0,
+    };
 }
 
 pub fn next(self: *Self) ?Token {
-    if (self.peeked) |peeked| {
-        self.peeked = null;
-        return peeked;
+    assert(self.peeked_count <= PEEK_BUFFER_SIZE);
+
+    if (self.peeked_count == 0) {
+        return self.tokenizer.next();
     }
-    return self.tokenizer.next();
+
+    const token = self.peeked_tokens[0];
+    // Shift items back
+    self.peeked_count -= 1;
+    for (0..self.peeked_count) |i| {
+        self.peeked_tokens[i] = self.peeked_tokens[i + 1];
+    }
+    return token;
 }
 
 pub fn peek(self: *Self) ?Token {
-    if (self.peeked) |peeked| {
-        return peeked;
+    return self.peekBy(1);
+}
+
+pub fn peekBy(self: *Self, count: usize) ?Token {
+    assert(count > 0);
+    assert(count <= PEEK_BUFFER_SIZE);
+    assert(self.peeked_count <= PEEK_BUFFER_SIZE);
+
+    for (self.peeked_count..count) |i| {
+        self.peeked_tokens[i] = self.tokenizer.next() orelse {
+            return null;
+        };
     }
-    self.peeked = self.tokenizer.next();
-    return self.peeked;
+    self.peeked_count = count;
+    return self.peeked_tokens[count - 1];
 }
 
 pub fn isEnd(self: *Self) bool {
