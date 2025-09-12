9b07f6419a3e4ef1e2fae09d6927904a82ff8a95
darcy <acednes@gmail.com>
2025-09-12 12:16:14 +1000

    fix: integer overflow with `TokenBuf`

--- src/parse/TokenBuf.zig
+++ src/parse/TokenBuf.zig
@@ -12,7 +12,6 @@ const Token = @import("Token.zig");
 const PEEK_BUFFER_SIZE = 2;
 
 tokenizer: Tokenizer,
-
 peeked_tokens: [PEEK_BUFFER_SIZE]Token,
 peeked_count: usize,
 
@@ -49,10 +48,12 @@ pub fn peekBy(self: *Self, count: usize) ?Token {
     assert(count <= PEEK_BUFFER_SIZE);
     assert(self.peeked_count <= PEEK_BUFFER_SIZE);
 
-    for (self.peeked_count..count) |i| {
-        self.peeked_tokens[i] = self.tokenizer.next() orelse {
-            return null;
-        };
+    if (self.peeked_count < count) {
+        for (self.peeked_count..count) |i| {
+            self.peeked_tokens[i] = self.tokenizer.next() orelse {
+                return null;
+            };
+        }
     }
     self.peeked_count = count;
     return self.peeked_tokens[count - 1];
